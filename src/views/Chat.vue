4f81e47565c46dd3fc84ba61c4ee164f72ab28c5
gmth6789 <gmth.6789@gmail.com>
2023-09-02 15:08:45 +0700

    main

---
+++ src/views/Chat.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="chat">
+    <h1>This is Chat page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/Rank.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="rank">
+    <h1>This is Rank page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
